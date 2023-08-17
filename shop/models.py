bc7e01feb2eb3d473ac83a027f03c7c7b09fa73b
aokealy <aokealy@gmail.com>
2023-08-17 08:55:41 +0000

    install pillow and uncommented image field and then make migrations

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
 class Category(models.Model):
     name = models.CharField(max_length=150, db_index=True)
     slug = models.SlugField(max_length=150, unique=True)
@@ -18,5 +19,11 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     slug = models.SlugField(max_length=255) 
-    # image = models.ImageField(upload_to='images/')   
+    image = models.ImageField(upload_to='images/')   
+
+    class Meta:
+        verbose_name_plural = 'products'
+
+    def __str__(self):
+        return self.title
     
