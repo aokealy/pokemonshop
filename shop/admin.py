eaea7d126c538b09d07f620d8b7f268f44838823
aokealy <aokealy@gmail.com>
2023-08-17 09:26:26 +0000

    change admin to class fields and prepopulate the fields for slug with the name and title

--- shop/admin.py
+++ shop/admin.py
@@ -5,5 +5,12 @@ from . models import Category, Product
 
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Product)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}    
