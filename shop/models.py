2a07017100cb6eb902795fbfa84c5fcf917fb69d
aokealy <aokealy@gmail.com>
2023-08-17 08:30:00 +0000

    create slug field and add a verbose to change category name and also add a str function to return the name of the object in admin panel

--- shop/models.py
+++ shop/models.py
@@ -5,3 +5,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=150, db_index=True)
     slug = models.SlugField(max_length=150, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name    
