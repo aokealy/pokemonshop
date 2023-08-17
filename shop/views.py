66b9eda468c259febbe0a9be5cd200a4918ef522
aokealy <aokealy@gmail.com>
2023-08-17 12:07:10 +0000

    create a function for categories so when we click all on categories it will show all pproducts so add it in settings context processors

--- shop/views.py
+++ shop/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 def shop(request):
 
     return render(request, 'shop/shop.html')
+
+
+def categories(request):
+
+    all_categories = Category.objects.all()
+
+    return {'all_categories': all_categories}
+
+
