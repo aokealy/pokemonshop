4aa83d74c2b1c1053361f553d8042143d3a69cee
aokealy <aokealy@gmail.com>
2023-08-17 11:45:40 +0000

    create a foreignkey that links the category with the product and add products throught the admin page

--- shop/models.py
+++ shop/models.py
@@ -14,6 +14,7 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=200)
     brand = models.CharField(max_length=150, default='un-branded') 
     description = models.TextField(blank=True)
