92da9fc1ff190a417f779d2f85dc4c427d33d976
aokealy <aokealy@gmail.com>
2023-08-18 13:13:13 +0000

    create the url's for each function in views

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,4 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
+    path('', views.checkout_summary, name='checkout-summary'),
+
+    path('add/', views.cart_checkout, name='checkout-add'),
+
+    path('delete/', views.checkout_delete, name='checkout-delete'),
+
+    path('update/', views.checkout_update, name='checkout-update'),
 ]
