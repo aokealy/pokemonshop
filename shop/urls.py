8730160766de02636dbd3ed674f7d4272f98cb7e
aokealy <aokealy@gmail.com>
2023-08-18 10:29:29 +0000

    create a list category view and url where it will filter the products based on their ccategories using the slug field name

--- shop/urls.py
+++ shop/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
 
     path('', views.shop, name='shop'),
     path('product/<slug:slug>/', views.product_information, name='product-information'),
+    path('search/<slug:slug>/', views.list_category, name='list-category'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -18,6 +18,15 @@ def categories(request):
     all_categories = Category.objects.all()
 
     return {'all_categories': all_categories}
+    
+
+def list_category(request, category_slug=None):
+
+    category = get_object_or_404(Category, slug=category_slug)
+
+    products = Product.objects.filter(category=category)
+
+    return render(request, 'shop/list-category.html', {'category':category, 'products':products})    
 
 
 def product_information(request, slug):
