066e3318fd2a3b101e0f34b9535e7dc78f313d63
aokealy <aokealy@gmail.com>
2023-08-18 15:58:52 +0000

    create a session key where returning user will have items in checkout bag or user will be new with nothing in shopping bag

--- checkout/checkout.py
+++ checkout/checkout.py
@@ -2,3 +2,12 @@ class Checkout():
 
     def __init__(self, request):
         self.session = request.session
+        
+        # returning user obtain existing session
+        checkout = self.session.get('session-key')
+
+        # new user generate a new session
+        if 'session-key' not in request.session:
+            checkout = self.session['session-key'] = {}
+
+        self.checkout = checkout
