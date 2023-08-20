a0d49191e298aefdaaa65b1e14bce518f8028499
aokealy <aokealy@gmail.com>
2023-08-20 11:30:07 +0000

    fix error in views by changing name from checkout to product and test using shell

--- checkout/views.py
+++ checkout/views.py
@@ -24,7 +24,7 @@ def checkout_add(request):
 
         checkout.add(product=product, product_qty=product_quantity)
 
-        response = JsonResponse({'this product is called': product.title, 'and the product quantity': checkout_quantity})
+        response = JsonResponse({'this product is called': product.title, 'and the product quantity': product_quantity})
 
         return response
 
