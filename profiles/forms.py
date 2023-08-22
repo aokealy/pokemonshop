66315b0d289b416f7189960b76d42b1a636793ba
aokealy <aokealy@gmail.com>
2023-08-22 10:05:24 +0000

    remove the help text that is default on usercreationform

--- profiles/forms.py
+++ profiles/forms.py
@@ -9,10 +9,16 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        help_texts = {
+            'username': None,
+            'email': None,
+        }
 
 
     def __init__(self, *args, **kwargs):
         super(CreateUserForm, self).__init__(*args, **kwargs) 
+        self.fields['password1'].help_text= None
+        self.fields['password2'].help_text= None
 
 
      # email validation form
