04bf9f01446780db3124752c1d8bb618c53a7548
aokealy <aokealy@gmail.com>
2023-08-22 09:41:22 +0000

    style the register page and add crispy forms in settings using bootstrap4

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 
+from .forms import CreateUserForm
+
 # Create your views here.
 
 def register(request):
-    return render(request,'profiles/registration/register.html')
+
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+
+            user = form.save()
+
+            return redirect('')
+
+    context = {'form':form}        
+
+    return render(request,'profiles/registration/register.html', context=context)
