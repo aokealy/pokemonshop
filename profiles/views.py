5ac8fcc213b909b17a7cc420ab0464ac94a122dc
aokealy <aokealy@gmail.com>
2023-08-23 15:47:29 +0000

    use session key so when user is either logged in or logged out that the items in checkout still exist and format it to pep8

--- profiles/views.py
+++ profiles/views.py
@@ -55,7 +55,16 @@ def my_login(request):
 
 # logout
 def profile_logout(request):
-    auth.logout(request)
+    try:
+        for key in list(request.session.keys()):
+            if key == "session_key":
+                continue
+
+            else:
+                del request.session[key]
+
+    except KeyError:
+        pass
 
     return redirect("shop")
 
