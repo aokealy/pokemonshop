f535217ad78895c843e0582a26046c834cf16cb6
aokealy <aokealy@gmail.com>
2023-08-24 09:26:33 +0000

    create a template files for payment failure and payment success and test the views to see if the templates render which they do

--- payment/urls.py
+++ payment/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 urlpatterns = [
 
+    path('payment-successful', views.payment_successful, name='payment-successful'),
 
-    
+    path('payment-failure', views.payment_failure, name='payment-failure'),
+
+
+
 ]
--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def payment_successful(request):
+    return render(request, 'payment/payment-successful.html')
+
+def payment_failure(request):
+    return render(request, 'payment/payment-failure.html')    
