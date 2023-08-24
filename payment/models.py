ea103fd03f9ab8eb148fe7b02267a81e4d2567c9
aokealy <aokealy@gmail.com>
2023-08-24 11:31:17 +0000

    create shipping address model where every field except zipcode is mandatory

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class ShippingAddress(models.Model):
+
+    full_name = models.CharField(max_length=250)
+    email = models.EmailField(max_length=250)
+    address1 = models.CharField(max_length=255)
+    address2 = models.CharField(max_length=255)
+    city = models.CharField(max_length=250)
+    zipcode = models.CharField(max_length=250, null=True, blank=True)
