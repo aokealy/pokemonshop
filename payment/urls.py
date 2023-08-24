d2a750f9aae09573f925766b2950b186b3267968
aokealy <aokealy@gmail.com>
2023-08-24 15:29:48 +0000

    create a final checkout html file and style it. Add views and url and create button on checkout summary page to link to final checkout page

--- payment/urls.py
+++ payment/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
 
+    path('final-checkout', views.final_checkout, name='final-checkout'),
+
     path('payment-successful', views.payment_successful, name='payment-successful'),
 
     path('payment-failure', views.payment_failure, name='payment-failure'),
--- payment/views.py
+++ payment/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def final_checkout(request):
+    return render(request, 'payment/final-checkout.html')
 
 def payment_successful(request):
     return render(request, 'payment/payment-successful.html')
