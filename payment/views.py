ca4be776694962782912980c9abd0616d0c63fae
aokealy <aokealy@gmail.com>
2023-08-26 11:17:33 +0000

    create a final checkout view to define if user is autenticated and that they have their shipping info prefilled. If no redirect to form to fill out either for authenticated user or guest user

--- payment/views.py
+++ payment/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+from .models import ShippingAddress
 
 # Create your views here.
 def final_checkout(request):
-    return render(request, 'payment/final-checkout.html')
+    # User with account - pre fill the form
+
+    if request.user.is_authenticated:
+        try:
+            # authenticated users with shipping info
+            shipping_address = ShippingAddress.objects.get(user=request.user.id)
+
+            context = {"shipping": shipping_address}
+
+            return render(request, "payment/final-checkout.html", context=context)
+
+        except:
+            # authenticated users with no shipping info
+            return render(request, "payment/final-checkout.html")
+    else:
+        # guest user
+         return render(request, 'payment/final-checkout.html')
 
 def payment_successful(request):
     return render(request, 'payment/payment-successful.html')
