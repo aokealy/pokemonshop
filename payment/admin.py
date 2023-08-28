04ce27f63c90f0d1b2d49e5e200fd3f1a7b55b0d
aokealy <aokealy@gmail.com>
2023-08-28 10:14:29 +0000

    add Purchase and PurchaseItem to admin.py page so they display in the admin page

--- payment/admin.py
+++ payment/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from . models import ShippingAddress
+from . models import ShippingAddress, Purchase, PurchaseItem
 # Register your models here.
 
 admin.site.register(ShippingAddress)
+admin.site.register(Purchase)
+admin.site.register(PurchaseItem)
 
