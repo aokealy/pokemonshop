fcc0256ec35c3c425b15a86acb8559964e200e6d
aokealy <aokealy@gmail.com>
2023-08-28 10:07:14 +0000

    create a purchase item model that will display all recent purchases based on the user by linking foreign keys

--- payment/models.py
+++ payment/models.py
@@ -38,3 +38,18 @@ class Purchase(models.Model):
         return 'Order - #' + str(self.id)
 
 
+class PurchaseItem(models.Model):
+    # Foreign Keys 
+    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+
+        return 'Product Item - #' + str(self.id)
+
+
+
+
