e6261efe3ea7bad18ca0b46c2041894ba3ab868a
aokealy <aokealy@gmail.com>
2023-08-28 11:56:11 +0000

    create the complete purchase view with shipping info

--- payment/views.py
+++ payment/views.py
@@ -30,5 +30,17 @@ def payment_failure(request):
     return render(request, 'payment/payment-failure.html')    
 
 
-def complete_purchase(reqiest):
-    pass
+def complete_purchase(request):
+    if request.POST.get("action") == "post":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        address1 = request.POST.get("address1")
+        address2 = request.POST.get("address2")
+        city = request.POST.get("city")
+        zipcode = request.POST.get("zipcode")
+
+        # this is to style the shipping address
+        shipping_address = (
+            address1 + "\n" + address2 + "\n" + city + "\n" + zipcode + "\n"
+        )
+    
