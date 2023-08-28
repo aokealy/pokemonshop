11c274c1151bd7317e7604c428bacfb64224d5f2
aokealy <aokealy@gmail.com>
2023-08-28 11:42:07 +0000

    use ajax for complete purchase where we have data stored of the fields of shipping address if user has entered that data beforehand and create a url and view for complete purchase

--- payment/urls.py
+++ payment/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
 
     path('final-checkout', views.final_checkout, name='final-checkout'),
 
+    path('complete-purchase', views.complete_purchase, name='complete-purchase'),
+
     path('payment-successful', views.payment_successful, name='payment-successful'),
 
     path('payment-failure', views.payment_failure, name='payment-failure'),
--- payment/views.py
+++ payment/views.py
@@ -21,8 +21,14 @@ def final_checkout(request):
         # guest user
          return render(request, 'payment/final-checkout.html')
 
+
 def payment_successful(request):
     return render(request, 'payment/payment-successful.html')
 
+
 def payment_failure(request):
     return render(request, 'payment/payment-failure.html')    
+
+
+def complete_purchase(reqiest):
+    pass
