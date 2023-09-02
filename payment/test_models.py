9930a6a2db004b121c82bc2e3b3de15034e7ebc0
aokealy <aokealy@gmail.com>
2023-09-02 12:08:51 +0000

    test models payment

--- payment/test_models.py
+++ payment/test_models.py
@@ -1,5 +1,10 @@
 from django.test import TestCase
-from .models import ShippingAddress, Purchase, PurchaseItem
+from payment.models import ShippingAddress, Purchase, PurchaseItem
+
+def test_shipping_address_string_method_returns_id(self):
+    shippingaddress = ShippingAddress.objects.create(full_name='test shippingaddress')
+    self.assertEquals(str(self.id), 'test shippingaddress')
+
 
 
 
